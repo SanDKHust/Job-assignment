a9ee1ab7dc1273e9444d93c52b2f03941a83907b
San <khacsan.hust@gmail.com>
2018-05-05 06:05:37 +0700

    a

--- app/src/main/java/vn/edu/hust/soict/khacsan/jobassignment/ui/group/GroupInfoActivity.java
+++ app/src/main/java/vn/edu/hust/soict/khacsan/jobassignment/ui/group/GroupInfoActivity.java
@@ -3,6 +3,8 @@ package vn.edu.hust.soict.khacsan.jobassignment.ui.group;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import vn.edu.hust.soict.khacsan.jobassignment.R;
+
 public class GroupInfoActivity extends AppCompatActivity {
 
     @Override
